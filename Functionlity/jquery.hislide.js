1c5b07e30e04f321afbd10d6ffb9e27dec454cbb
Nuhu_Mandy <caljr9301@gmail.com>
2022-08-24 15:14:04 +0300

    What I do first release not responsive

---
+++ Functionlity/jquery.hislide.js
@@ -0,0 +1,61 @@
+(function($) {
+
+    var slide = function(ele,options) {
+        var $ele = $(ele);
+        var setting = {
+            speed: 2000,
+            interval: 4000,        
+        };
+
+        $.extend(true, setting, options);
+
+        var states = [
+            { $zIndex: 3, width: 300, height: 500, top: 230, left: 20, $opacity: 0.7 },
+            { $zIndex: 4, width: 650, height: 930, top: 0, left: 260, $opacity: 1 },
+            { $zIndex: 3, width: 300, height: 500, top: 230, left: 860, $opacity: 0.7 },
+        ];
+
+        var $lis = $ele.find('li');
+        var timer = null;
+
+        $ele.find('.hi-next').on('click', function() {
+            next();
+        });
+        $ele.find('.hi-prev').on('click', function() {
+            states.push(states.shift());
+            move();
+        });
+        $ele.on('mouseenter', function() {
+            clearInterval(timer);
+            timer = null;
+        }).on('mouseleave', function() {
+            autoPlay();
+        });
+
+        move();
+        autoPlay();
+
+
+        function move() {
+            $lis.each(function(index, element) {
+                var state = states[index];
+                $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
+            });
+        }
+
+        function next() {
+            states.unshift(states.pop());
+            move();
+        }
+
+        function autoPlay() {
+            timer = setInterval(next, setting.interval);
+        }
+    }
+    $.fn.hiSlide = function(options) {
+        $(this).each(function(index, ele) {
+            slide(ele,options);
+        });
+        return this;
+    }
+})(jQuery);
